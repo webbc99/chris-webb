89290c647ac86489aa3a112c7869615f4ee3325c
Aerodyne <webbc99@gmail.com>
2019-01-05 20:45:57 +0000

    Added game download link and a couple of newer song iframes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,12 @@ class HomeController < ApplicationController
   def home
   end
 
+  def download_cwtd
+    send_file(
+      "#{Rails.root}/public/cwtd.exe",
+      filename: "cwtd.exe",
+      type: "application/exe"
+      )
+  end
+
 end
