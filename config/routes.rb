cd30d0de5ec64c80a5ecdfb42dbbb22936ceb478
Aerodyne <webbc99@gmail.com>
2019-05-21 17:44:42 +0100

    Added projects and contact pages, and a lot more links

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#home'
   get 'download_cwtd', to: 'home#download_cwtd'
+  get 'projects', to: 'projects#index'
+  get 'contact', to: 'contact#contact'
 end
